ccdd7296818113795b8e4c3c119f17451c9d9cb4
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2023-02-21 11:16:36 +0100

    Implemented Add note and remove note. build.gradle update

--- app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -1,25 +1,31 @@
 package com.example.jetnote.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetnote.R
 import com.example.jetnote.components.NoteInputText
+import com.example.jetnote.data.NotesDataSource
 import com.example.jetnote.model.Note
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun NoteScreen(
@@ -33,6 +39,7 @@ fun NoteScreen(
     var description by remember {
         mutableStateOf("")
     }
+    val context = LocalContext.current
     Column(modifier = Modifier.padding(6.dp)) {
         TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }, actions = {
             Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon")
@@ -66,20 +73,56 @@ fun NoteScreen(
                 text = "Save",
                 onClick = {
                     if (title.isNotEmpty() && description.isNotEmpty()) {
-                        //save/add to the list
+                        onAddNote(Note(title = title, description = description))
                         title = ""
                         description = ""
+                        Toast.makeText(context, "Note added", Toast.LENGTH_SHORT).show()
                     }
                 })
+        }
+        Divider(modifier = Modifier.padding(10.dp))
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClick = {
+                    onRemoveNote(note)
+                })
+
+            }
         }
     }
 
 }
 
+@Composable
+fun NoteRow(modifier: Modifier = Modifier, note: Note, onNoteClick: (Note) -> Unit) {
+    Surface(
+        modifier = modifier
+            .padding(4.dp)
+            .clip(shape = RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFdfE6Eb),
+        elevation = 6.dp,
+    ) {
+        Column(
+            modifier = modifier
+                .clickable { onNoteClick(note)}
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = note.title, style = MaterialTheme.typography.subtitle2)
+            Text(text = note.description, style = MaterialTheme.typography.subtitle1)
+            Text(
+                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, dd/MM/yyyy")),
+                style = MaterialTheme.typography.caption
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
 }
 
 @Composable
